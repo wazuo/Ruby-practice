ea938c518335c3633c47ef25e4fe7d60a6db8efb
EC2 Default User <ec2-user@ip-172-31-12-133.ec2.internal>
2021-12-04 13:38:43 +0000

    授業ファイル

---
+++ luby/appple.rb
@@ -0,0 +1,5 @@
+apple ="Nagano"
+
+if apple =="Aomori"
+  puts　"このリンゴは青森県産ではなく、長野県産です。"
+end
---
+++ luby/compared.rb
@@ -0,0 +1,8 @@
+total = 100
+if total <200
+   puts"合計は 200未満です"
+end
+
+if total >=150
+  puts "合計は150以上です"
+end
